e6047edc8f74ae9e9322458f0539c292aecc258b
Nick Patrick <nick.patrick@gmail.com>
2025-01-16 10:24:44 -0600

    Initial Commit

---
+++ Alphabetizer/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Alphabetizer
+//
+//  Created by Nick Patrick on 1/12/25.
+//
+
+import Foundation
+
+enum Message: String {
+    case instructions = "Place the tiles in alphabetical order"
+    case tryAgain = "Almost 😅"
+    case youWin = "You win ! 🏆"
+}
